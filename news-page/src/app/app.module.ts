bebbd82b71932aa2876599cfaf41b83a12767fc3
Alexandr Ogarkov <Alexandr_Ogarkov@epam.com>
2019-02-05 18:54:42 +0300

    Add grid layout

--- news-page/src/app/app.module.ts
+++ news-page/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NewsItemComponent } from './components/news-item/news-item.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsListComponent,
+    NewsItemComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
