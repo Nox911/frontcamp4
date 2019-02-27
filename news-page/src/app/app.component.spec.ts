daeef23710f68027fcde75e2d1b27beaf0dc654a
Alexandr Ogarkov <Alexandr_Ogarkov@epam.com>
2019-02-27 19:15:40 +0300

    Add some unit tests

--- news-page/src/app/app.component.spec.ts
+++ news-page/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent, FooterComponent } from './components/index';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +10,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'news-page'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('news-page');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to news-page!');
   });
 });
--- news-page/src/app/components/news-item/news-item.component.spec.ts
+++ news-page/src/app/components/news-item/news-item.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { NewsItemComponent } from './news-item.component';
 
@@ -6,8 +8,21 @@ describe('NewsItemComponent', () => {
   let component: NewsItemComponent;
   let fixture: ComponentFixture<NewsItemComponent>;
 
+  const mockNewsItem = {
+    id: 2,
+    author: 'Nox',
+    title: 'Bitcoin Approaches $3,600 Again as Top Cryptocurrencies See Gains',
+    description: 'Bitcoin (BTC) is hovering near $3,600 again, according to Coin360 data.',
+    // tslint:disable-next-line:max-line-length
+    urlToImage: 'https://s.yimg.com/uu/api/res/1.2/3kO5xSCdp_BGbpCRJs2nQw--~B/aD00OTQ7dz03NDA7c209MTthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en-US/cointelegraph_667/98cecdffd4a9d789109b068328f5f14a',
+    url: 'https://www.businessinsider.com/martin-lewis-drops-lawsuit-against-facebook-over-bitcoin-scam-ads-2019-1',
+    publishedAt: '2019-01-23T11:24:00Z',
+    content: 'As 2019 throws off the cobwebs of early January',
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [ NewsItemComponent ]
     })
     .compileComponents();
@@ -16,6 +31,7 @@ describe('NewsItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewsItemComponent);
     component = fixture.componentInstance;
+    component.newsItem = mockNewsItem;
     fixture.detectChanges();
   });
 
